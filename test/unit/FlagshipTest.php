864f1df5394e6059133afdbb6f940398c54317f4
William Comnisky <william.comnisky@vestiairecollective.com>
2020-03-09 19:36:47 +0100

    Added Campaign Activation

--- test/unit/FlagshipTest.php
+++ test/unit/FlagshipTest.php
@@ -7,6 +7,7 @@ namespace Wcomnisky\Flagship\UnitTests;
 use PHPUnit\Framework\TestCase;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 use Symfony\Contracts\HttpClient\ResponseInterface;
+use Wcomnisky\Flagship\Api\RequestParameters;
 use Wcomnisky\Flagship\Context\ContextInterface;
 use Wcomnisky\Flagship\Flagship;
 
@@ -37,8 +38,8 @@ class FlagshipTest extends TestCase
      * @covers \Wcomnisky\Flagship\Flagship::__construct
      * @covers \Wcomnisky\Flagship\Flagship::getSingleCampaignUrl
      * @covers \Wcomnisky\Flagship\Flagship::replaceNamedParameter
-     * @uses \Wcomnisky\Flagship\Api\RequestParameters::getMode()
-     * @uses \Wcomnisky\Flagship\Api\RequestParameters::isTriggerHitEnabled()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::getMode
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::isTriggerHitEnabled
      * @uses \Wcomnisky\Flagship\Api\RequestParameters::getDecisionGroup()
      * @uses \Wcomnisky\Flagship\Api\RequestParameters::isFormatResponseEnabled()
      */
@@ -74,6 +75,141 @@ class FlagshipTest extends TestCase
 
         $flagship = new Flagship($environmentId, $httpClient);
         $returnedResponse = $flagship->requestSingleCampaign($visitorId, $campaignId, $context);
+        $this->assertSame($response, $returnedResponse);
+    }
+
+    /**
+     * @covers \Wcomnisky\Flagship\Flagship::requestAllCampaigns
+     * @covers \Wcomnisky\Flagship\Flagship::__construct
+     * @covers \Wcomnisky\Flagship\Flagship::getAllCampaignsUrl
+     * @covers \Wcomnisky\Flagship\Flagship::replaceNamedParameter
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::isDefaultMode()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::getMode()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::isTriggerHitEnabled()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::getDecisionGroup()
+     */
+    public function test_successful_requestAllCampaigns_with_no_request_parameters()
+    {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $context = $this->createMock(ContextInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $environmentId = 'my_environment_id';
+        $visitorId = 'my_visitor_id';
+
+        $httpClient->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->identicalTo('POST'),
+                $this->identicalTo("https://decision-api.flagship.io/v1/{$environmentId}/campaigns"),
+                $this->callback(function ($options) use ($visitorId) {
+                    $expectedOptions = [
+                        'json' => [
+                            'visitor_id' => $visitorId,
+                            'context' => [],
+                            'decision_group' => null,
+                            'trigger_hit' => true
+                        ]
+                    ];
+
+                    return $options === $expectedOptions;
+                })
+            )->willReturn($response);
+
+        $flagship = new Flagship($environmentId, $httpClient);
+        $returnedResponse = $flagship->requestAllCampaigns($visitorId, $context);
+        $this->assertSame($response, $returnedResponse);
+    }
+
+    /**
+     * @covers \Wcomnisky\Flagship\Flagship::requestAllCampaigns
+     * @covers \Wcomnisky\Flagship\Flagship::__construct
+     * @covers \Wcomnisky\Flagship\Flagship::getAllCampaignsUrl
+     * @covers \Wcomnisky\Flagship\Flagship::replaceNamedParameter
+     * @covers \Wcomnisky\Flagship\Flagship::setRequestParameters
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::isDefaultMode()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::getMode()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::isTriggerHitEnabled()
+     * @uses \Wcomnisky\Flagship\Api\RequestParameters::getDecisionGroup()
+     */
+    public function test_successful_requestAllCampaigns_with_mode_simple_should_append_query_string_to_URL()
+    {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $context = $this->createMock(ContextInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+        $requestParameters = $this->createMock(RequestParameters::class);
+
+        $environmentId = 'my_environment_id';
+        $visitorId = 'my_visitor_id';
+
+        $httpClient->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->identicalTo('POST'),
+                $this->identicalTo("https://decision-api.flagship.io/v1/{$environmentId}/campaigns?mode=simple"),
+                $this->callback(function ($options) use ($visitorId) {
+                    $expectedOptions = [
+                        'json' => [
+                            'visitor_id' => $visitorId,
+                            'context' => [],
+                            'decision_group' => null,
+                            'trigger_hit' => true
+                        ]
+                    ];
+
+                    return $options === $expectedOptions;
+                })
+            )->willReturn($response);
+
+        $requestParameters->expects($this->once())
+            ->method('getMode')
+            ->willReturn('simple');
+
+        $requestParameters->expects($this->once())
+            ->method('isTriggerHitEnabled')
+            ->willReturn(true);
+
+        $flagship = new Flagship($environmentId, $httpClient);
+        $flagship->setRequestParameters($requestParameters);
+        $returnedResponse = $flagship->requestAllCampaigns($visitorId, $context);
+        $this->assertSame($response, $returnedResponse);
+    }
+
+    /**
+     * @covers \Wcomnisky\Flagship\Flagship::requestCampaignActivation
+     * @covers \Wcomnisky\Flagship\Flagship::__construct
+     */
+    public function test_successful_requestCampaignActivation()
+    {
+        $httpClient = $this->createMock(HttpClientInterface::class);
+        $response = $this->createMock(ResponseInterface::class);
+
+        $environmentId = 'my_environment_id';
+        $visitorId = 'my_visitor_id';
+        $variationGroupId = 'my_variation_group_id';
+        $variationId = 'my_variation_id';
+
+        $httpClient->expects($this->once())
+            ->method('request')
+            ->with(
+                $this->identicalTo('POST'),
+                $this->identicalTo('https://decision-api.flagship.io/v1/activate'),
+                $this->callback(function ($options) use ($environmentId, $visitorId, $variationGroupId, $variationId) {
+                    $expectedOptions = [
+                        'json' => [
+                            'vid' => $visitorId,
+                            'cid' => $environmentId,
+                            'caid' => $variationGroupId,
+                            'vaid' => $variationId
+                        ]
+                    ];
+
+                    return $options === $expectedOptions;
+                })
+            )->willReturn($response);
+
+        $flagship = new Flagship($environmentId, $httpClient);
+        $returnedResponse = $flagship->requestCampaignActivation($visitorId, $variationGroupId, $variationId);
         $this->assertSame($response, $returnedResponse);
     }
 }
